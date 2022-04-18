7b62022f67455ef9082304a571a3bad1b34a63a3
benTheBlaster382 <builongvu2001@gmail.com>
2022-04-18 17:34:44 +0700

    add codelab1.2a

---
+++ Codelab 1.2/Codelab1.2a-HelloToastChallenge/app/src/main/java/com/example/hellotoastchallenge/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.hellotoastchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount;
+    private TextView mshowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mshowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void CountUp(View view) {
+        mCount++;
+        if (mshowCount != null) {
+            mshowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
